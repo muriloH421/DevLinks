1cd6af57995849584a39cfa337babe6550b58263
muriloH421 <basiliomurilo421@gmail.com>
2023-08-10 13:31:09 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 var toggleMode = () => {
-    const html =document.documentElement;
-    html.classList.toggle("ligth")
-};
+  const html = document.documentElement;
+
+  html.classList.toggle("ligth");
+
+  const isligth = html.classList.contains("ligth");
+  const srcValue =
+isligth
+? "../img/perfil.jpg"
+: "../img/perfil.jpg"
+  const altValue =
+  isligth
+   ? "imagem de perfil com um avatar na cor preto"
+   : "imagem de perfil com avatar na cor vermelha"
+
+document.querySelector("#profile img")
+.setAttribute("src",srcValue)
+}
